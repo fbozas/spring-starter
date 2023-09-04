5bdd9498019cda361de682ec8104babd39671dd6
fbozas <fbozas@hotmail.com>
2023-09-04 19:06:57 +0300

    Added Project Files

---
+++ src/main/java/gr/aueb/cf/springstarter/SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterApplication.class, args);
+    }
+
+}
