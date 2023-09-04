fcdf8c362d9236a60bcb733da0f9574989a013e0
fbozas <fbozas@hotmail.com>
2023-09-04 20:33:02 +0300

    Added REST controller with a simple method

---
+++ src/main/java/gr/aueb/cf/springstarter/rest/HelloRestController.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.springstarter.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class HelloRestController {
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello Rest CF!";
+    }
+}
