b37a5c2d5aba786f9df59c78b6fdb7ced80d30ce
fbozas <fbozas@hotmail.com>
2023-09-04 19:36:45 +0300

    Pass data from controller to the view

--- src/main/java/gr/aueb/cf/springstarter/controller/HelloController.java
+++ src/main/java/gr/aueb/cf/springstarter/controller/HelloController.java
@@ -1,9 +1,12 @@
 package gr.aueb.cf.springstarter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/hello-app")
@@ -12,5 +15,11 @@ public class HelloController {
     @GetMapping("/hello-world")
     public String getHello(){
         return "index";
+    }
+
+    @GetMapping("/get-date")
+    public  String getDate(Model model){
+      model.addAttribute("dateTxt", new Date());
+      return "date";
     }
 }
