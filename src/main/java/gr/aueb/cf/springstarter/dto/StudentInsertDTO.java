f206071695ef4c753ce5a8046e584df2b976f5af
fbozas <fbozas@hotmail.com>
2023-09-07 12:33:46 +0300

    Added email on StudentInsertDTO & removed bean validation in 2 fields

--- src/main/java/gr/aueb/cf/springstarter/dto/StudentInsertDTO.java
+++ src/main/java/gr/aueb/cf/springstarter/dto/StudentInsertDTO.java
@@ -1,31 +1,32 @@
 package gr.aueb.cf.springstarter.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class StudentInsertDTO {
 
-    @NotBlank
-    @Size(min = 3, max = 50)
+    @Email
+    private String email;
+
+//    @NotBlank
+//    @Size(min = 3, max = 50)
     private String firstname;
 
-    @NotBlank
-    @Size(min = 3, max = 50)
+//    @NotBlank
+//    @Size(min = 3, max = 50)
     private String lastname;
 
-    public String getFirstname() {
-        return firstname;
-    }
-
+    public String getFirstname() { return firstname; }
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
