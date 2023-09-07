337b0133ba585e4de63efa4539f77ed3bb008db3
fbozas <fbozas@hotmail.com>
2023-09-07 11:55:23 +0300

    Added validator package & added annotations on DTO

--- src/main/java/gr/aueb/cf/springstarter/dto/StudentInsertDTO.java
+++ src/main/java/gr/aueb/cf/springstarter/dto/StudentInsertDTO.java
@@ -1,7 +1,16 @@
 package gr.aueb.cf.springstarter.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class StudentInsertDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String firstname;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String lastname;
 
     public String getFirstname() {
